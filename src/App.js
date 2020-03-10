5fbefd5a549e2efefcf8bf67b22d3b09043f94f5
BenzNatchapon <48754390+BenzNatchapon@users.noreply.github.com>
2020-03-10 22:43:27 +0700

    add src

--- src/App.js
+++ src/App.js
@@ -10,13 +10,26 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 const localizer = Calendar.momentLocalizer(moment);
 const DnDCalendar = withDragAndDrop(Calendar);
 
+const MyCalendar = props => (
+  <div>
+    <Calendar
+      localizer={localizer}
+      events={this.state.events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
+);
+
 class App extends Component {
   state = {
     events: [
       {
         start: new Date(),
         end: new Date(moment().add(1, "days")),
-        title: "First Event"
+        title: "First Event",
+        allDay: true
       },
       {
         start: new Date(),
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import Basic from "./examples/basic";
+//import Try from "./examples/tryToUseReact";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
