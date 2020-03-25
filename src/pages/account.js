393a35f70535708b434249b328c4dd5622f36619
vachiraa <vachira.spt@gmail.com>
2020-03-26 03:48:34 +0700

    Almost done

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,29 +1,43 @@
 import React from 'react'
 const Account = () => {
     return (
-        <div class="columns">
-            <div class="column">
-                <p class="title">ACCOUNT</p>
-                <p class="titile">First name</p>
-                <div class="content">
-                    <input style={{ background: "#FFEAEA" }} class="input is-rounded is-large" type="text" placeholder="Firstname"></input>
-                    <p class="title">Last name</p>
-                    <div class="content" >
-                        <input style={{ background: "#FFEAEA" }} class="input is-rounded is-large" type="text" placeholder="Last name"></input>
+        <div class="columns" style={{ padding: 30 }}>
+            <div class="column is-two-thirds" style={{ paddingLeft: 30 }}>
+                <p class="title is-size-1">ACCOUNT</p>
+                <div class="card" style={{ borderRadius: "25px" }}>
+                    <div class="card-content">
+                        <p class="title">CMU Account</p>
+                        <p class="title has-text-grey is-size-4">studyP@cmu.ac.th</p>
+                        <p class="title">First name</p>
+                        <p class="title has-text-grey is-size-4">myfirstname</p>
+                        <p class="title">Last name</p>
+                        <p class="title has-text-grey is-size-4">mylastname</p>
                         <p class="title">Student Code</p>
-                        <div class="content" >
-                            <input style={{ background: "#FFEAEA" }} class="input is-rounded is-large" type="text" placeholder="Number"></input>
-                        </div>
-                        <button style={{ backgroundColor: "#ffc30b"}} class="button is-large has-text-white">EDIT</button>
+                        <p class="title has-text-grey is-size-4">600610xxx</p>
+                        <p class="title">Faculty</p>
+                        <p class="title has-text-grey is-size-4">engineering</p>
+                        <button style={{ backgroundColor: "#ffc30b" }} class="button is-large has-text-white">EDIT</button>
+                    </div>
+                    <div class="card-content">
+                        <button class="button is-large has-text-white is-danger">LOG OUT</button>
                     </div>
                 </div>
             </div>
-            <div class="column">
-                Second column
-                <button style={{ backgroundColor: "#ffc30b"}} class="button is-large has-text-white">UPLOAD YOUR PICTURE</button>
-                <button style={{ backgroundColor: "#ffc30b"}} class="button is-large has-text-white">LOG OUT</button>
+
+            <div class="column" style={{ paddingTop: 50 }}>
+                {/* <div class="card">
+                <div class="card-content">
+                    <div class="card-image" >
+                        <img src="https://is4-ssl.mzstatic.com/image/thumb/Purple113/v4/b6/83/63/b683639f-bd63-47f7-a2d9-6d89e93b6776/source/256x256bb.jpg"></img>
+                    </div>
+                    <button style={{ backgroundColor: "#ffc30b" }} class="button is-large has-text-white">UPLOAD YOUR PICTURE</button>
+                </div>
+                <div class="card-content">
+                
+                </div>
+            </div> */}
             </div>
-        </div>
+        </div >
     )
 }
 export default Account;
