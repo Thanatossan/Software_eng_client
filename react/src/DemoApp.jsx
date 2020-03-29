3d00507afc48a96f67255dd67b9f7916bab4a554
BenzNatchapon <48754390+BenzNatchapon@users.noreply.github.com>
2020-03-30 04:33:50 +0700

    more beautiful calendar

--- react/src/DemoApp.jsx
+++ react/src/DemoApp.jsx
@@ -1,71 +1,82 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction' // needed for dayClick
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
 
-import './main.scss'
-
+import "./main.scss";
 
 export default class DemoApp extends React.Component {
-
-  calendarComponentRef = React.createRef()
+  calendarComponentRef = React.createRef();
   state = {
     calendarWeekends: true,
-    calendarEvents: [ // initial event data
-      
+    calendarEvents: [
+      // initial event data
     ]
-  }
+  };
 
   render() {
     return (
-      <div className='demo-app' >
-        <div className='demo-app-top'>
-        
+      <div className="demo-app">
+        <div className="demo-app-top">
           <h1 center> Calendar </h1>
-         
         </div>
-        <div className='demo-app-calendar' >
+        <div className="demo-app-calendar">
           <FullCalendar
             defaultView="dayGridMonth"
             header={{
-              left: 'prev,next today',
-              center: 'title',
-              right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
             }}
-            plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
-            ref={ this.calendarComponentRef }
-            weekends={ this.state.calendarWeekends }
-            events={ this.state.calendarEvents }
-            dateClick={ this.handleDateClick }
-            />
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            ref={this.calendarComponentRef}
+            weekends={this.state.calendarWeekends}
+            events={this.state.calendarEvents}
+            dateClick={this.handleDateClick}
+          />
         </div>
+        {/* <table>
+          <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+          </tr>
+          <tr>
+            <td>Peter</td>
+            <td>Griffin</td>
+          </tr>
+          <tr>
+            <td>Lois</td>
+            <td>Griffin</td>
+          </tr>
+        </table> */}
       </div>
-    )
+    );
   }
-
 
   toggleWeekends = () => {
-    this.setState({ // update a property
+    this.setState({
+      // update a property
       calendarWeekends: !this.state.calendarWeekends
-    })
-  }
+    });
+  };
 
   gotoPast = () => {
-    let calendarApi = this.calendarComponentRef.current.getApi()
-    calendarApi.gotoDate('2000-01-01') // call a method on the Calendar object
-  }
+    let calendarApi = this.calendarComponentRef.current.getApi();
+    calendarApi.gotoDate("2000-01-01"); // call a method on the Calendar object
+  };
 
-  handleDateClick = (arg) => {
-    if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
-      this.setState({  // add new event data
-        calendarEvents: this.state.calendarEvents.concat({ // creates a new array
+  handleDateClick = arg => {
+    if (confirm("Would you like to add an event to " + arg.dateStr + " ?")) {
+      this.setState({
+        // add new event data
+        calendarEvents: this.state.calendarEvents.concat({
+          // creates a new array
           title: prompt(name),
           start: arg.date,
           allDay: arg.allDay
         })
-      })
+      });
     }
-  }
-
+  };
 }
