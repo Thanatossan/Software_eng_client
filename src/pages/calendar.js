dbd38bc28fa9d9106e9f2506a04258f8762b9103
Thanatossan <kotokikung@gmail.com>
2020-04-17 23:40:08 +0700

    add redux to todolist and calendar

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -17,26 +17,27 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
-
+import { connect } from "react-redux";
 import "../components/calendar_components/main.scss";
 var url = "http://13.76.181.113/api/calendar/topic/";
 var url2 = "http://13.76.181.113/api/calendar/created/";
-var studentCode = "600610780";
-export default class calendar extends React.Component {
+
+class calendar extends React.Component {
   calendarComponentRef = React.createRef();
   state = {
     calendarWeekends: true,
     calendarEvents: [
       // initial event data
-      { id: "", title: "", start: "" }
-    ]
+      { id: "", title: "", start: "" },
+    ],
   };
+
   componentDidMount() {
-    fetch(url + studentCode)
-      .then(response => response.json())
-      .then(jsonData => {
+    fetch(url + this.props.student_code)
+      .then((response) => response.json())
+      .then((jsonData) => {
         this.setState({ calendarEvents: jsonData });
-        // console.log(jsonData);
+        console.log(jsonData);
         // this.setState({
         //   // add new event data
         //   calendarEvents: this.state.calendarEvents.concat({
@@ -56,12 +57,13 @@ export default class calendar extends React.Component {
         <div className="demo-app-calendar">
           <br></br>
           <br></br>
+
           <FullCalendar
             defaultView="dayGridMonth"
             header={{
               left: "prev,next today",
               center: "title",
-              right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
             }}
             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
             ref={this.calendarComponentRef}
@@ -100,7 +102,7 @@ export default class calendar extends React.Component {
   toggleWeekends = () => {
     this.setState({
       // update a property
-      calendarWeekends: !this.state.calendarWeekends
+      calendarWeekends: !this.state.calendarWeekends,
     });
   };
 
@@ -109,7 +111,7 @@ export default class calendar extends React.Component {
     calendarApi.gotoDate("2000-01-01"); // call a method on the Calendar object
   };
 
-  handleDateClick = arg => {
+  handleDateClick = (arg) => {
     // eslint-disable-next-line no-restricted-globals
     var newEvent = prompt(
       "Create an event in " + arg.dateStr + "\n Enter title"
@@ -123,9 +125,17 @@ export default class calendar extends React.Component {
           id: "",
           title: newEvent,
           start: arg.date,
-          allDay: arg.allDay
-        })
+          allDay: arg.allDay,
+        }),
       });
     }
   };
 }
+const mapStateToProps = function (state) {
+  return {
+    message: "This is message from mapStateToProps",
+    student_code: state.student_code,
+  };
+};
+const withConnect = connect(mapStateToProps)(calendar);
+export default withConnect;
