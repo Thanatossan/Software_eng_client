3a4bd727cfaf62419eee1c0bf4a16a6d237efa8d
Thanatossan <kotokikung@gmail.com>
2020-04-18 04:04:15 +0700

    fix schedule add map

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -8,40 +8,30 @@ var isne_scd = "http://13.76.181.113:4000/api/schedule/get/isne";
 const Schedule = () => {
   const [CPE, setCPE] = useState([]);
   const [ISNE, setISNE] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     fetch(cpe_scd)
-      .then(res => res.json())
-      .then(
-        (CPE) => {
-          setIsLoaded(true);
-          setCPE(CPE);
-        }
-      )
+      .then((res) => res.json())
+      .then((CPE) => {
+        setCPE(CPE);
+      });
     fetch(isne_scd)
-      .then(res => res.json())
-      .then(
-        (ISNE) => {
-          setIsLoaded(true);
-          setISNE(ISNE);
-        }
-      )
-  });
-
-  var cpe_obj = JSON.stringify(CPE)
-  var cpe_data = JSON.parse(cpe_obj)
-
-  var isne_obj = JSON.stringify(ISNE)
-  var isne_data = JSON.parse(isne_obj)
-
+      .then((res) => res.json())
+      .then((ISNE) => {
+        setISNE(ISNE);
+      });
+  }, []);
   return (
     <div class="container">
       <h1>SUBJECT SCHEDULE</h1>
-      <Select/>
-      <span>{cpe_obj}</span>
-      <hr></hr>
-      <span>{isne_obj}</span>
+      {/* {CPE[0] === undefined ? <span></span> : <span>{CPE[0].semester}</span>} */}
+      {/* <hr></hr>
+      <span>{ISNE}</span> */}
+      {CPE.map((cpe, index) => (
+        <div>
+          <h2>{index}</h2>
+        </div>
+      ))}
     </div>
   );
 };
